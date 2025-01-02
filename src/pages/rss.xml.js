68ee1e1a59f17972d6c0629b72814d30d8ca641c
Myles Lewando <myles.lewando@opendataservices.coop>
2025-01-02 10:13:07 +0000

    Fix RSS generation

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,29 +1,34 @@
-import rss from '@astrojs/rss';
+import { experimental_AstroContainer as AstroContainer } from 'astro/container';
+import { getContainerRenderer as getMDXRenderer } from '@astrojs/mdx';
+import { loadRenderers } from 'astro:container';
 import { getCollection } from 'astro:content';
-import sanitizeHtml from 'sanitize-html';
-import MarkdownIt from 'markdown-it';
-const parser = new MarkdownIt();
+import rss from '@astrojs/rss';
 
 export async function GET(context) {
-  const notes = await getCollection('notes');
-  return rss({
-    title: 'Code Macabre',
-    description: 'Notes from Myles Lewando',
-    site: context.site,
-    xmlns: {
-      atom: "http://www.w3.org/2005/Atom",
-      dc: "http://purl.org/dc/elements/1.1/"
-    },
-    items: notes.map((note) => ({
-      title: note.data.title,
-      pubDate: note.data.date,
-      description: note.data.description,
-      link: `/notes/${note.slug}/`,
-      content: sanitizeHtml(parser.render(note.body).replace('src="/', `src="${context.site}`).replace('href="/', `href="${context.site}`), {
-        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['code', 'pre', 'img'])
-      }),
-      customData: '<dc:creator>Myles Lewando</dc:creator>',
-    })),
-    customData: `<atom:link href="${context.site}rss.xml" rel="self" type="application/rss+xml" /><language>en-gb</language>`,
-  });
+    const renderers = await loadRenderers([getMDXRenderer()]);
+    const container = await AstroContainer.create({ renderers });
+    const notes = await getCollection('notes');
+
+    const items = [];
+    for (const note of notes) {
+        const { Content } = await note.render();
+        let content = await container.renderToString(Content);
+        const link = new URL(`/notes/${note.slug}`, context.url.origin).toString();
+        const customData = '<dc:creator>Myles Lewando</dc:creator>';
+        
+        content = content.replace('src="/', `src="${context.site}`).replace('href="/', `href="${context.site}`);
+        items.push({ ...note.data, link, customData, content });
+    }
+
+    return rss({
+        title: 'Code Macabre',
+        description: 'Notes from Myles Lewando',
+        site: context.site,
+        xmlns: {
+          atom: 'http://www.w3.org/2005/Atom',
+          dc: 'http://purl.org/dc/elements/1.1/'
+        },
+        items,
+        customData: `<atom:link href="${context.site}rss.xml" rel="self" type="application/rss+xml" /><language>en-gb</language>`
+    });
 }
